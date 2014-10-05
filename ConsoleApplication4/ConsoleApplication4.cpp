f76478f25d62bfd63e02aad6a1ffc65b711e3009
kirill2485 <kirill2485@gmail.com>
2014-10-04 22:09:31 -0700

    GitHub Test
    Just checking if GitHub is committing properly

---
+++ ConsoleApplication4/ConsoleApplication4.cpp
@@ -0,0 +1,17 @@
+//Kirill's AI, version 0.01 (Alpha)
+
+#include "stdafx.h"
+#include <string>
+#include <iostream>
+
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+	double version = 0.01;
+	std::cout << "Welcome to Kirill's AI, The current version you are using is " << version << std::endl;
+	std::string kirill = "kirill";
+	//git test
+	//git test 1
+	std::cout << kirill << std::endl;
+}
+
